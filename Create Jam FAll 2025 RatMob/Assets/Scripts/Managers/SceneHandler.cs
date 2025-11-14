a2c12e43bef1c6ea9c0058f55d9752094b4c3830
panthydev <panthydev@gmail.com>
2025-11-15 00:59:32 +0100

    made a scene handler
    can reload scene and load new scene by index

---
+++ Create Jam FAll 2025 RatMob/Assets/Scripts/Managers/SceneHandler.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour
+{
+    public static SceneHandler instance;
+    public int currentSceneIndex { get; private set; }
+    private void Awake()
+    {
+        //singleton pattern
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void LoadScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+        currentSceneIndex = sceneIndex;
+    }
+
+    public void ReloadScene()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+    
+    
+    
+
+}
